426fb5f30751e667f318468d1ada7ce25d3fb2f7
Sebastian Koczotowski <sebastian.koczotowski@gmail.com>
2018-05-10 11:19:55 +0200

    Updated. Levels

--- Labirynth.py
+++ Labirynth.py
@@ -12,7 +12,12 @@ realrows=[]
 for i in range (19):
     realrows.append([" "]*62)
 
-board1 = [(0,0,60), (18,0,61), (0,0,18), (0,60,18), (14,0,40), (10,15,30), (2,25,13), (7,12,32), (3,15,30), (5,0,20), (8,3,18)]
+level1 = [(0,0,60), (18,0,61), (0,0,18), (0,60,18), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0)]
+level2 = [(0,0,60), (18,0,61), (0,0,18), (0,60,18), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0)]
+level3 = [(0,0,60), (18,0,61), (0,0,18), (0,60,18), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0)]
+level4 = [(0,0,60), (18,0,61), (0,0,18), (0,60,18), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0)]
+level5 = [(0,0,60), (18,0,61), (0,0,18), (0,60,18), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0)]
+level6 = [(0,0,60), (18,0,61), (0,0,18), (0,60,18), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0), (0,0,0)]
 
 #Funkcja rysujaca poziome sciany
 def wall_horizontal(x,y,z):
@@ -20,7 +25,7 @@ def wall_horizontal(x,y,z):
         length = i
         row = x
         column = y + i
-        realrows[row][column] = "%"
+        realrows[row][column] = "▇"
 
 #Funkcja rysujaca pionowe sciany
 def wall_vertical(x,y,z):
@@ -28,7 +33,7 @@ def wall_vertical(x,y,z):
         length = i
         row = x + i
         column = y
-        realrows[row][column] = "%"
+        realrows[row][column] = "▇"
 
 # Funkcja odpowiadjaca za czyszczenie ekranu
 def clear_screen():
@@ -49,7 +54,7 @@ def move_player_in_direction(action,direction):
         heigth += 1
     elif action is "move" and direction is "up":
         heigth -= 1
-    if realrows[heigth][width] != "%":
+    if realrows[heigth][width] != "▇":
         realrows[heigth][width] = "⛄"
     else:
         if action is "move" and direction is "right":
@@ -64,17 +69,25 @@ def move_player_in_direction(action,direction):
 
 # Funkcja rysujaca plansze gry
 def board(arg):
+    #SCIANY Zewnetrzne
         wall_horizontal(*arg[0])
         wall_horizontal(*arg[1])
         wall_vertical(*arg[2])
         wall_vertical(*arg[3])
+    #SCIANY Wewnetrzne Poziome
         wall_horizontal(*arg[4])
         wall_horizontal(*arg[5])
-        wall_vertical(*arg[6])
+        wall_horizontal(*arg[6])
         wall_horizontal(*arg[7])
         wall_horizontal(*arg[8])
         wall_horizontal(*arg[9])
-        wall_horizontal(*arg[10])
+    #SCIANY Wewnetrzne Pionowe
+        wall_vertical(*arg[10])
+        wall_vertical(*arg[11])
+        wall_vertical(*arg[12])
+        wall_vertical(*arg[13])
+        wall_vertical(*arg[14])
+        wall_vertical(*arg[15])
         realrows[12][10] = "☯"
         for s in realrows:
                 print(*s, sep = "")
@@ -122,7 +135,7 @@ while q:
         else:
                 clear_screen()
 
-        board(board1)
+        board(level1)
 
 
 
