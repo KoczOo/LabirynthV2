c3d49ca4acf35aa685e6cef5d04177f286b1c6e1
Albert Pyrgies <albert@wysoka.org>
2018-05-10 10:51:28 +0200

    Updated mindblowing labirynth wall drawing function

--- Labirynth.py
+++ Labirynth.py
@@ -12,6 +12,7 @@ realrows=[]
 for i in range (19):
     realrows.append([" "]*62)
 
+board1 = [(0,0,60), (18,0,61), (0,0,18), (0,60,18), (14,0,40), (10,15,30), (2,25,13), (7,12,32), (3,15,30), (5,0,20), (8,3,18)]
 
 #Funkcja rysujaca poziome sciany
 def wall_horizontal(x,y,z):
@@ -62,18 +63,18 @@ def move_player_in_direction(action,direction):
         realrows[heigth][width] = "⛄"
 
 # Funkcja rysujaca plansze gry
-def board():
-        wall_horizontal(0,0,60)
-        wall_horizontal(18,0,61)
-        wall_vertical(0,0,18)
-        wall_vertical(0,60,18)
-        wall_horizontal(14,0,40)
-        wall_horizontal(10,15,30)
-        wall_vertical(2,25,13)
-        wall_horizontal(7,12,32)
-        wall_horizontal(3,15,30)
-        wall_horizontal(5,0,20)
-        wall_horizontal(8,3,18)
+def board(arg):
+        wall_horizontal(*arg[0])
+        wall_horizontal(*arg[1])
+        wall_vertical(*arg[2])
+        wall_vertical(*arg[3])
+        wall_horizontal(*arg[4])
+        wall_horizontal(*arg[5])
+        wall_vertical(*arg[6])
+        wall_horizontal(*arg[7])
+        wall_horizontal(*arg[8])
+        wall_horizontal(*arg[9])
+        wall_horizontal(*arg[10])
         realrows[12][10] = "☯"
         for s in realrows:
                 print(*s, sep = "")
@@ -121,7 +122,7 @@ while q:
         else:
                 clear_screen()
 
-        board()
+        board(board1)
 
 
 
